5d48d1eb10be5965009edb28565ab636165e0692
Max Botwin <maxmarbot@gmail.com>
2021-10-21 12:55:25 -0500

    this time

--- server/index.js
+++ server/index.js
@@ -4,9 +4,9 @@ const path = require('path')
 const app = express()
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(_dirname, "../index.html"))
+    res.sendFile(path.join(__dirname, "../index.html"))
 })
 
 const port = process.env.PORT || 4567
 
-app.listen(4567, console.log(`Server is running on ${port}`))
+app.listen(port, console.log(`Server is running on ${port}`))
